004ad05773fb09adbad0c5cd9813b20b6c1b93e8
Geno Patterson <genosis@yahoo.com>
2011-05-28 11:25:57 -0500

    blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f299f675fd4512035a532a9ec31af2b9235b7d3502a6075815b5275def682a4bcec9368ed51c54c1c330b8263426148bf68c8ebcc588463e567a5c284b912f2b'
