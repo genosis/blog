ed142f257e3ed71e634abea30acae6ebdf0b3057
Geno Patterson <genosis@yahoo.com>
2011-06-24 19:11:59 -0500

    Adding some tests

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -5,4 +5,31 @@ class PostTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+  
+  test "body is present" do
+    post = Post.new :title => "Hello, World!"
+    assert !post.valid?
+  end
+  
+  test "title is present" do
+    post = Post.new :body => "Hello, World!"
+    assert !post.valid?
+  end
+  
+  test "title is unique" do
+     post = Post.new :title => "First Post", :body => "hello,World!"
+     assert !post.valid?
+  end
+  
+  test "title length" do
+     post = Post.new :title => "", :body => "hello,World!"
+     assert !post.valid?, "Title is too short"
+     
+     post.title = "z" * 21
+     assert !post.valid?, "Title is too long"
+  end
+
+  
+  
+  
 end
